91f823a14ffb439eda78e7184f004a2d4cf62a34
Adam Monsen <haircut@gmail.com>
2023-01-06 21:36:26 -0800

    initial commit

---
+++ Readme.md
@@ -0,0 +1,62 @@
+# ow
+
+ow - Rudimentary Nextcloud command-line client 🦉
+
+## Usage
+
+### Get internal link
+
+Given a local file path sync'd by the Nextcloud desktop client, return the Internal link on the Nextcloud server.
+
+```bash
+$ ow pl ~/Nextcloud/test.md
+https://cloud.example.com/f/229
+```
+
+## Installation
+
+NOTE: only works on Linux so far
+
+* install prerequisites
+    * Python 3
+    * Nextcloud desktop sync client
+* download the `main.py` script, save it as `ow`
+* edit values under `YOUR CONFIG`
+* put the script in your path, for example: `~/bin/ow`
+* make sure it is executable
+
+## Architecture
+
+ow talks with the [Nextcloud WebDAV API](https://docs.nextcloud.com/server/latest/developer_manual/client_apis/WebDAV/basic.html).
+
+## History
+
+ow started with one command: `pl`. Hopefully by the time you are reading this it has learned more tricks. It began as a small, working proof of concept to see if I and others use this, like it, and want to do more with it. If we choose to continue working on it, here are some ideas on what we might do:
+
+## Other ideas
+
+* add more features
+    * post chat messages
+    * add a task
+    * add a calendar event
+    * search
+* improve setup
+    * automate install
+* improve config
+    * read and use config from Nextcloud desktop client
+    * [read and use secrets from desktop/OS password manager](https://pypi.org/project/keyring/) or smartcard
+* improve code maintainability
+    * use a real argument parser
+    * add tests
+* improve cross-platform compatibility
+    * add Windows support
+    * add macOS support
+* pick a better name
+* use a real contributor agreement
+* Are there other/better free software utilities like this one? List/promote them.
+* Would it make more sense to implement this and the other feature ideas (above) within the [official client](https://docs.nextcloud.com/desktop/latest/advancedusage.html)? Do the maintainers want these features?
+
+## COPYLEFT AND LICENSE
+
+* Copyright ©2023 Adam Monsen <haircut@gmail.com>
+* License: AGPL v3 or later (see COPYING)
---
+++ main.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+
+# ow - Nextcloud command-line client
+# Copyright (C) 2023  Adam Monsen
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import re
+import requests
+import sys
+import xml.etree.cElementTree as cET
+
+##########################################################################
+# INTERNAL CONFIG - don't change these
+##########################################################################
+
+nextcloudWebdavFileRoot = 'remote.php/dav/files'
+
+##########################################################################
+# YOUR CONFIG - change these
+##########################################################################
+
+nextcloudServer = 'http://localhost:8080'
+
+# where files are sync'd locally
+localSyncFolder = '/home/user/Nextcloud'
+
+# root of localSyncFolder on Nextcloud server
+remoteDestinationFolder = '/'
+
+nextcloudUsername = 'admin'
+
+# if you use multi-factor auth, use an app password here
+nextcloudPassword = 'admin'
+
+##########################################################################
+# MAIN CODE - probably leave this alone unless you wanna hack
+##########################################################################
+
+if len(sys.argv) != 3:
+    print('Usage: {} pl FILE'.format(sys.argv[0]), file=sys.stderr)
+    sys.exit(1)
+
+if sys.argv[1] != 'pl':
+    print('Sorry, I only know one trick so far. Care to teach me another?', file=sys.stderr)
+    sys.exit(1)
+
+if not os.path.exists(sys.argv[2]):
+    print('path does not exist', file=sys.stderr)
+    sys.exit(1)
+
+realpath = os.path.realpath(sys.argv[2])
+
+# remove localSyncFolder from realpath
+relativePathOnly = re.sub(localSyncFolder, '', realpath)
+
+# munge what's left into fileurl
+fileurl = '/'.join([nextcloudServer, nextcloudWebdavFileRoot, nextcloudUsername, relativePathOnly])
+
+_propfindBody = '''<?xml version="1.0" encoding="UTF-8"?>
+<d:propfind xmlns:d="DAV:">
+  <d:prop xmlns:oc="http://owncloud.org/ns">
+    <oc:fileid/>
+  </d:prop>
+</d:propfind>'''
+
+try:
+    response = requests.request('PROPFIND', fileurl, auth=(nextcloudUsername, nextcloudPassword), data=_propfindBody)
+except requests.RequestException as e:
+    print('PROPFIND request failed: {}'.format(e), file=sys.stderr)
+    sys.exit(1)
+
+# response status code must be between 200 and 400 to continue
+if not response:
+    print('HTTP response code {}. Response text: {}'.format(response.status_code, response.text), file=sys.stderr)
+    sys.exit(1)
+
+root = cET.fromstring(response.text)
+fileId = root.findtext('.//{http://owncloud.org/ns}fileid')
+
+if fileId is None:
+    print('HTTP response code {}. Response text: {}'.format(response.status_code, response.text), file=sys.stderr)
+    sys.exit(1)
+
+print('{}/f/{}'.format(nextcloudServer, fileId))
